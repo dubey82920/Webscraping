9b72f7459d9cf85b9f28d68b039077b1a87fd6da
dubey82920 <abhishekkumardubey107@gmail.com>
2023-03-21 21:35:19 +0530

    second Commit

--- assignment/views.py
+++ assignment/views.py
@@ -2,34 +2,28 @@ from django.shortcuts import render,HttpResponse
 import requests
 from bs4 import BeautifulSoup
 from .models import Product
-# from assignment import models
+# from assignment import models  
 
 def scrape_product(request):
-    product_details = []
-    url=[]
+    product_list = Product.objects.all()
+    for product in product_list:
+        url = product.url
+        print(url)
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # loop through product URLs and extract product details
-    # for url in product_urls:
-        # send GET request and parse HTML with BeautifulSoup
+        description = soup.find('div', {'id': 'productDescription'}).get_text().strip()
+        asin = soup.find('span', {'class': 'a-text-bold'}).get_text().strip()
+        product_description = soup.find('div', {'id': 'productDescription'}).get_text().strip()
+        manufacturer = soup.find('th', text='Manufacturer').find_next_sibling('td').get_text().strip()
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url='https://www.amazon.in//ADISA-BP005-Weight-Casual-Backpack/dp/B07F3X45WZ'
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # print(soup.prettify)
-    results=soup.find_all('div',{'class':'a-section feature detail-bullets-wrapper bucket'})
-    print(results,"results")
-    # asin = soup.find('span', {'class': 'a-text-bold'}).text
-    
-    description = soup.find('div')
-    print(description)
-    # asin = soup.find('span', {'class': 'a-icon-alt'})#.find_next_sibling('td').get_text().strip()
-    # product_description=soup.find('div', {'id': 'product_Description'}).get_text().strip()
-    # manufacturer = soup.find('th', text='Manufacturer').find_next_sibling('td').get_text().strip()
-    # print(description,asin,product_description,manufacturer)
-    # print("ASIN", asin)
-    
-    return HttpResponse("updated")    
+        Product.objects.filter(url=url).update(asin=asin, description=description, product_description=product_description, manufacturer=manufacturer)
+
+    return HttpResponse("updated")
+
+
+
 
 def scrape_products(request):
     base_url = 'https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_{}'
@@ -44,7 +38,7 @@ def scrape_products(request):
             price = float(product.find('span', {'class': 'a-price-whole'}).text.replace(',', ''))           
             rating = float(product.find('span', {'class': 'a-icon-alt'}).text.split()[0])
             reviews = float(product.find('span', {'class': 'a-size-base'}).text.replace(',', '').split()[0])
-            
+
             Product.objects.create(url=url, name=name, price=price, rating=rating, reviews=reviews)
         
     return HttpResponse("added")
